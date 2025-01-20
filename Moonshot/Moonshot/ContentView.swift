23179e0a3069a071d662920d40af2d5473332a61
Carlos Eduardo Witte <cewitte@gmail.com>
2025-01-20 00:00:41 -0300

    added Mission View and updated README.md with screenshot

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -22,8 +22,53 @@ struct ContentView: View {
     
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        Text(String(astronauts.count))
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    
+                                    Text(mission.formmattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                            
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle(Text("Moonshot"))
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+        }
     }
 }
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Moonshot
+
 ## Project 8
+
 An app that lets users learn about the missions and astronauts that formed NASA’s Apollo space program (by Paul Hudson - @twostraws).
+
+![Mission View UI](/mission_view.png)
