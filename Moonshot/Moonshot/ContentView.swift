19132867dd453e1dc513a862a15ecdffa01f065a
Carlos Eduardo Witte <cewitte@icloud.com>
2025-01-24 21:50:45 -0300

    removed unnecessary code from previous exemples

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -5,16 +5,6 @@
 //  Created by Carlos Eduardo Witte on 05/10/24.
 //
 
-//struct User: Codable {
-//    let name: String
-//    let address: Address
-//}
-//
-//struct Address: Codable {
-//    let street: String
-//    let city: String
-//}
-
 import SwiftUI
 
 struct ContentView: View {
